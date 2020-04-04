6fa7140511353201354513b4540e3c1bd2f8584b
ShadowDrone <shadowdrone@hotmail.com>
2020-04-04 11:37:58 -0300

    Subo metodos agregados a temporada y hago un ejemplo de casteo y redefinicion en App.java

--- Ejercicios/02_OOP/Pooflix/src/app/App.java
+++ Ejercicios/02_OOP/Pooflix/src/app/App.java
@@ -1,23 +1,63 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static Scanner Teclado = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
 
         Pooflix miPooflix = new Pooflix();
 
-        System.out.println("Iniciando Catalogo");
+        System.out.println("Iniciando Catalogo....");
 
         miPooflix.inicializarCatalogo();
 
-        Serie bb = miPooflix.buscarSerie("Breaking Bad");
+        Serie bb = miPooflix.buscarSerie("The Walking Dead");
 
-        Temporada laMejorTemporada = bb.getTemporada(5);
+        Temporada laMejorTemporada = bb.getTemporada(4);
+
+        Episodio epiDemo;
+
+        int posicion = 0;
+
+        /// En base a un numero de posicion de episodio, obtenerlo de la lista
+        System.out.println("Ingrese la posicion");
+
+        posicion = Teclado.nextInt();
+
+        epiDemo = laMejorTemporada.getEpisodioAtPosicion(posicion);
+
+        epiDemo.reproducir();
+
+        // instanceof: devuelve si la variable es de tipo Clase X
+        // En este caso, devuelve si epiDemo declarado como Episodio apunta a una clase
+        // Websodio
+        if (epiDemo instanceof Websodio) {
+            System.out.println("Estoy reproduciendo un websodio");
+
+            // (clase): Operador casteo.
+            // Casteo lo que hace es acomodar la variable a castear, a una clase especifica
+            // da error si no se puede acomodar. por eso siempre se pregunta antes con un
+            // instance of
+            Websodio webso = (Websodio) epiDemo;
+            webso.enviarAlertaDeQueEstaOnline();
+        } else
+            System.out.println("Estoy reproduciendo un Episodio");
 
         Episodio elTop;
 
-        elTop = laMejorTemporada.getEpisodio(7);
+        elTop = laMejorTemporada.getEpisodio(1);
 
         elTop.reproducir();
+
+        Episodio otroEpi;
+
+        otroEpi = laMejorTemporada.getEpisodio(9);
+
+        otroEpi.reproducir();
+
     }
 
 }
--- Ejercicios/02_OOP/Pooflix/src/app/Temporada.java
+++ Ejercicios/02_OOP/Pooflix/src/app/Temporada.java
@@ -10,6 +10,12 @@ public class Temporada {
     public int numero;
     public List<Episodio> episodios = new ArrayList<>();
 
+    /**
+     * Obtiene un episodio en base a su numero de episodio
+     * 
+     * @param nro
+     * @return
+     */
     public Episodio getEpisodio(int nro) {
         // Recorrer cada episodio
         // Si el nro de episodio del ciclo es igual a "nro"
@@ -23,5 +29,59 @@ public class Temporada {
         }
 
         return null;
+    }
+
+    /**
+     * Obtiene un episodio en base al nombre (es una sobrecarga del metodo
+     * getEpisodio(int nro))
+     * 
+     * @param nombre
+     * @return
+     */
+    public Episodio getEpisodio(String nombre) {
+        // Recorrer cada episodio
+        // Si el nombre de episodio del ciclo es igual a "nombre"
+        // Devolver esa episodio
+
+        for (Episodio epi : this.episodios) {
+            if (epi.nombre.equalsIgnoreCase(nombre)) {
+                return epi;
+            }
+
+        }
+
+        return null;
+    }
+
+    /**
+     * Obtiene un episodio en base a la posicion dentro del ArrayList. Recordar que
+     * los arraylist la primer posicion es la 0
+     * 
+     * @param pos
+     * @return
+     */
+    public Episodio getEpisodioAtPosicion(int pos) {
+        return this.episodios.get(pos);
+    }
+
+    /**
+     * Obtiene el nro de posicion(no el nro de episodio) de un episodio dentro de la
+     * lista
+     * 
+     * @param nro
+     * @return
+     */
+    public int getEpisodioPosition(int nro) {
+
+        int pos = 0;
+        for (Episodio epi : this.episodios) {
+            if (epi.numero == nro) {
+                return pos;
+            }
+            pos++;
+        }
+        if (this.episodios.size() == pos)
+            return -1;
+        return pos;
     }
 }
